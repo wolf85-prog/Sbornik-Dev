c20f9205de525c1f5a7221d964e693380e3a4d2c
Sergey Gornih <seregaland@yandex.ru>
2025-04-05 19:05:28 +0300

    fix menu

--- components/ui/PopupMenu.js
+++ components/ui/PopupMenu.js
@@ -1,30 +1,50 @@
-import { Modal, SafeAreaView, TouchableOpacity, View, Text, StyleSheet } from "react-native";
+import { Modal, SafeAreaView, TouchableOpacity, View, Text, StyleSheet, Animated, Easing } from "react-native";
 import { Ionicons, FontAwesome, Entypo } from '@expo/vector-icons';
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const PopupMenu = (options) => {
     const [visible, setVisible] = useState(false)
-
+    const scale = useRef(new Animated.Value(0)).current
+    function resizeBox(to) {
+        to === 1 && setVisible(true);
+        Animated.timing(scale, {
+            toValue: to,
+            useNativeDriver: true,
+            duration: 200,
+            easing: Easing.linear,
+        }).start(()=>to === 0 && setVisible(false))
+    }
     
     return (
         <>
             <TouchableOpacity
-                onPress={()=>setVisible(true)}
+                onPress={()=>resizeBox(1)}
                 style={{marginRight: 10}}
             >
                 <Entypo name="dots-three-vertical" size={22} color="white" />
             </TouchableOpacity>
 
             <Modal transparent visible={visible}>
-                <SafeAreaView style={{flex: 1}} onTouchStart={()=> setVisible(false)}>
-                    <View style={[styles.popup, {padding: 10}]}>
+                <SafeAreaView style={{flex: 1}} onTouchStart={()=> resizeBox(0)}>
+                    <Animated.View style={[
+                        styles.popup, 
+                        {
+                            padding: 10
+                        },
+                        // {
+                        //     opacity: scale.interpolate({inputRange: [0,1], outputRange: [0,1]}),
+                        // },
+                        // {
+                        //     transform: [{scale}],
+                        // }
+                        ]}>
                         {options.options.map((op, i)=> (
                             <TouchableOpacity style={[styles.option]} key={i} onPress={()=>op.action}>
                                 <Text style={styles.text}>{op.title}</Text>
                                 {/* <Icon name="" size="" color="" /> */}
                             </TouchableOpacity>
                         ))}
-                    </View>
+                    </Animated.View>
                 </SafeAreaView>
             </Modal>
         </>
